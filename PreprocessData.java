f4359b002baad562b1ec6c39dd4c5f583c2bd62e
SUBHADIP JANA <subhadip1997a@gmail.com>
2017-09-17 19:54:26 +0530

    Commited

--- PreprocessData.java
+++ PreprocessData.java
@@ -1,10 +1,11 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class PreprocessData
 {
 	String classListFile;
 	String rawDataSetFile;
-	String outputDataSetFile;
+	String processedDataFile;
 	String line;
 	String[] lineSplit;
 	BufferedReader bfr;
@@ -13,7 +14,7 @@ public class PreprocessData
 	{
 		this.classListFile = classListFile;
 		this.rawDataSetFile = rawDataSetFile;
-		this.outputDataSetFile = outputDataSetFile;
+		this.processedDataFile = outputDataSetFile;
 		try
 		{
 			bfr = new BufferedReader(new FileReader(classListFile));
@@ -24,31 +25,44 @@ public class PreprocessData
 		}
 	}		
 	
-	/*
+	
 	public static void main(String[] args) {
 		PreprocessData P = new PreprocessData("C://Users//SUBHADIP JANA//Desktop//classList.txt","C://Users//SUBHADIP JANA//Desktop//candidateDataSet.txt","C://Users//SUBHADIP JANA//Desktop//newData.txt");
 		try{
 		P.modifyDataSet();
+		P.createDataPointsList();
 		}
 		catch(IOException ioe)
 		{
 			ioe.printStackTrace();
 		}		
 	}
-	*/
+	
 
 	public void modifyDataSet() throws IOException
 	{
 		bfr = new BufferedReader(new FileReader(rawDataSetFile));
-		FileWriter fw = new FileWriter(outputDataSetFile);		
+		FileWriter fw = new FileWriter(processedDataFile);		
 		while((line=bfr.readLine())!=null)
 		{
 			lineSplit = line.split(",");
 			lineSplit[0] = "";
 			fw.write(line.substring(line.indexOf(',')+1) + "\n");			
 		}
-		fw.write("\b ");
 		fw.close();
-	}	
+	}
+	
+	public String[][] createDataPointsList() throws IOException
+	{
+		ArrayList<String[]> dataPoints_ArrayList = new ArrayList<String[]>();
+		bfr = new BufferedReader(new FileReader(processedDataFile));
+		
+		while((line=bfr.readLine())!=null)
+		{
+			dataPoints_ArrayList.add(line.split(","));
+		}
+		
+		return dataPoints_ArrayList.toArray(new String[0][0]);
+	}
 
 }
